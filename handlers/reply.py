168c74c3c12f339aad5288679fa8fb110fbc4f75
SAR087 <68345593+SAR087@users.noreply.github.com>
2021-03-03 11:52:26 +0530

    Update reply.py

--- handlers/reply.py
+++ handlers/reply.py
@@ -4,12 +4,15 @@ from helpers import reply as r
 
 def reply(update, context):
     try:
+        msg = r(
+
+update.effective_message.text
+
+            )
         context.bot.send_chat_action(update.effective_chat.id, "typing")
         update.effective_message.reply_text(
-            r(
-                update.effective_message.text
-            )
-        )
+          msg
+)
     except:
         pass
 
